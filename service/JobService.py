077ca4b95de6ada501624805a353efbfdd569d1a
Zou1c <1094381158@qq.com>
2022-08-04 22:22:12 +0800

    add salaryPredict function

--- service/JobService.py
+++ service/JobService.py
@@ -1,4 +1,5 @@
 from dao.JobDao import JobDao
+from sklearn.naive_bayes import MultinomialNB
 
 class JobService():
 
@@ -109,4 +110,60 @@ class JobService():
 
         return job, sjobList
         pass
+
+
+    def predictSalary(self, predictInput):
+        jobDao = JobDao()
+        lowSalary = 0
+        highSalary = 0
+        try:
+            condition = jobDao.getPDC()  #  [{'jobCity':'xx','jobType':'aaa'},{'jobCity':'XX','jobType':'bbb'}]
+            salary  = jobDao.getJobSalary()
+            print(condition)
+            print('-'*30)
+
+            y1 = []
+            y2 = []
+            # 结果以k为单位做标签
+            for i in range(len(salary)):
+                y1.append( int(salary[i].get('jobLowSalary') / 1000) )
+                y2.append( int(salary[i].get('jobHighSalary') / 1000) )
+
+            Dict1 = {}
+            Dict2 = {}
+            index1 = 1
+            index2 = 1
+            # 统计与转换
+            for i in range(len(condition)):
+                city = condition[i].get('jobCity')
+                jobType = condition[i].get('jobType')
+                if Dict1.get(city) == None:
+                    Dict1[city] = index1
+                    index1 += 1
+                if Dict2.get(jobType) == None:
+                    Dict2[jobType] = index2
+                    index2 += 1
+
+            xCount = []
+            for i in range(len(condition)):
+                v1 = Dict1.get(condition[i].get('jobCity'))
+                v2 = Dict2.get(condition[i].get('jobType'))
+                pair = [v1,v2]
+                xCount.append(pair)
+
+
+            # 调库预测  [[1,2]]广州，大数据,r:20-40； [[2,2]]北京，Java;r:
+            test = [[5,2]]
+            bayers = MultinomialNB()
+            bayers.fit(xCount, y1)
+            result = bayers.predict(test)
+            print('lowsalary',result)
+            bayers.fit(xCount, y2)
+            result = bayers.predict(test)
+            print('highsalary',result)
+
+        finally:
+            jobDao.close()
+
+        return lowSalary, highSalary
     pass
